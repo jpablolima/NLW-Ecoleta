c08938c32a4e69038591b868e07fcc97618dd185
João Pablo <jp.pablolima@gmail.com>
2020-06-10 13:36:40 -0300

    refatoração

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const server = express();
 
+// Pasta Publica
 server.use(express.static("public"));
 
+
+
+// Template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true
+});
+
+
+
+
+
+// Rotas
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 });
 
-server.get("/create-point", (req, res) =>{
-  res.sendFile(__dirname + "/views/create-point.html");
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
 });
 
+server.get("/search", (req, res) =>{
+  return res.render("search-results.html");
+});
+
+
+// Port Server
 server.listen(3000);
